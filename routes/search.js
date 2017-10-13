a2c2a594c41fa652b51fc95ccc423192e647e650
panforrest <guoqianp@gmail.com>
2017-10-12 23:24:47 -0400

    V1 11:20 search route parse itunes api

---
+++ routes/search.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var superagent = require('superagent')
+
+/* GET users listing. */
+router.get('/:term', function(req, res, next) {
+  var term = req.params.term
+
+  var url = 'http://itunes.apple.com/search'
+
+  superagent
+  .get(url)
+  .query({media:'podcast', term:term})
+  .set('Accept', 'application/json')
+  .end(function(err, response){
+    if (err){
+      res.json({
+      	confirmation: 'fail',
+      	message: err
+      })
+      return
+    }
+
+    console.log(JSON.stringify(response))
+
+	res.json({
+	  confirmation: 'success',
+	  response: response.text
+	 })  	
+  })
+
+  // res.json({
+  // 	confirmation: 'success',
+  // 	term: term
+  // })
+
+
+  // res.send('respond with a resource');
+});
+
+module.exports = router;
