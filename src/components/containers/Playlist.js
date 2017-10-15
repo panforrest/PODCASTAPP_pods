101dc3bcff9189cc2739243dfb063b1bfd45a2a5
panforrest <guoqianp@gmail.com>
2017-10-15 00:02:02 -0400

    V4 6:30 my attempt to console log itunes json from Playlist/Search component searchBox

--- src/components/containers/Playlist.js
+++ src/components/containers/Playlist.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 import { Search } from '../presentation'
+import superagent from 'superagent'
 
 class Playlist extends Component {
+
+  searchPodcasts(event){
+    if (event.keyCode != 13)
+      return
+
+    console.log('searchPosdcasts: '+event.target.value)//NOT console.log('searchPosdcasts: '+event.keyCode)
+    var url = 'http://localhost:3000/search/' + event.target.value
+    console.log('url: '+url)
+
+    superagent
+    .get(url)
+    .query(null)
+    // .set('Accept', 'json/application')
+    .end((err, data) => {  //THE ORDER OF err, data SHOULD NOT SWITCH
+      	if (err) {
+      	  alert(err)
+      	  return
+      	}
+      	console.log(JSON.stringify(data.body))
+      })
+
+  }
+
   render(){
+
   	return(
   	  <div>	
 	  	<div className="hero-header bg-shop animated fadeindown">
 	      <h1 className="hero-title">Play</h1>
 	    </div>
-	    <Search /> 
+	    <Search onSearch={this.searchPodcasts.bind(this)}/> 
 	  </div>    
   	)
   }
