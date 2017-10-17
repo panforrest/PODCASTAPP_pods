2ac67b565e78e9b6db3421de49e466221bf274f7
panforrest <guoqianp@gmail.com>
2017-10-16 20:35:23 -0400

    V7 4:37 fetch the playlist audio feeds of a prodcast when the user selects a podcast from the menu

--- src/components/containers/Podcasts.js
+++ src/components/containers/Podcasts.js
@@ -1,19 +1,22 @@
+// <a onClick={this.selectPodcast.bind(this, "podcast")} href="#"> //WRONG
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import actions from '../../actions' 
 
 class Podcasts extends Component {
 
+  selectPodcast(podcast, event){
+    // console.log('selectPodcast: '+JSON.stringify(this.props.podcast))
+    console.log('selectPodcast: '+JSON.stringify(podcast))
+  }
+
   render(){
-
     const list = this.props.podcasts.all || []
-
-
   	return(
       <div>
         { list.map((podcast, i) => {
             return <div key={podcast.trackId} className="shop-banner animated fadeinup delay-2">
-              <a href="#">
+              <a onClick={this.selectPodcast.bind(this, podcast)} href="#">
                 <img src={podcast.artworkUrl60} alt="" />
                 <div className="opacity-overlay valign-wrapper">
                   <div className="valign center width-100">
